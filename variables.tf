a4325bbe97ce8aa202910f1c41841b5daa4f913f
Danie de Jager <36914577+danieldejager@users.noreply.github.com>
2022-02-16 09:29:37 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,11 +5,9 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
 }
 
